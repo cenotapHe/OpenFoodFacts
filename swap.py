36f310855cf3812e8f470c44d990ea53944b6631
cenotapHe <bat1ste@hotmail.com>
2018-05-23 16:29:23 +0200

    Version functional

---
+++ swap.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+os.system('cls')
+
+print("q - Quitter")
+print("Appuyer sur n'importe quelle autre touche pour continuer d'utiliser ce programme.")
+swap_answer = input(">>> ")
+
+if swap_answer == "q":
+	os.system('cls')
+	sys.exit(0)
+
+else:
+	os.system('cls')
+	os.system("py -3.4 main.py")
+	sys.exit(0)
