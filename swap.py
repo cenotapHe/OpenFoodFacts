b25819e5b489c3e0d2c0e75d92089b6d484173c4
cenotapHe <bat1ste@hotmail.com>
2018-05-23 16:47:58 +0200

    Revert "test for revert"
    This reverts commit c23473447d8af8c0cf5380a0f24f11bc6d36eb58.

--- swap.py
+++ swap.py
@@ -8,10 +8,10 @@ print("Appuyer sur n'importe quelle autre touche pour continuer d'utiliser ce pr
 swap_answer = input(">>> ")
 
 if swap_answer == "q":
-    os.system('cls')
-    sys.exit(0)
+	os.system('cls')
+	sys.exit(0)
 
 else:
-    os.system('cls')
-    os.system("py -3.4 main.py")
-    sys.exit(0)
+	os.system('cls')
+	os.system("py -3.4 main.py")
+	sys.exit(0)
